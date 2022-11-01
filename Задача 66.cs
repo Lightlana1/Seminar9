d99c009b16d6846705aa187f79b7a87d71e786eb
Lightlana1 <113245900+Lightlana1@users.noreply.github.com>
2022-11-02 00:07:37 +0300

    Задача 66

---
+++ Задача 66.cs
@@ -0,0 +1,23 @@
+﻿void Zadacha66()
+{
+    // Задайте значения M и N. Напишите рекурсивный метод, который найдёт сумму натуральных элементов в промежутке от M до N.
+    Console.WriteLine("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    SumNum(m, n);
+
+    void SumNum(int m, int n, int sum = 0)
+    {
+        if (m >= n+1)
+        {
+            Console.WriteLine($"{sum}");
+            return;
+        }
+        sum = sum + m;
+        m++;
+        SumNum(m, n, sum);
+    }
+}
+Zadacha66();
