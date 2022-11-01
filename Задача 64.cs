e3f8b763f9b3b1d3f322c35314ce8478b2caf417
lightlana <lebendaufdensternen@gmail.com>
2022-11-01 22:51:41 +0300

    Задача 64

---
+++ Задача 64.cs
@@ -0,0 +1,25 @@
+﻿
+void Zadacha64()
+//Задайте значения M и N. Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+{
+    Console.WriteLine("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    FindNatural(m, n);
+    void FindNatural(int m, int n)
+    {
+        if (m % 3 == 0)
+        {
+            Console.Write(m + " ");
+        }
+        m++;
+        if(m >= n+1)
+        {
+            return;
+        }
+        FindNatural(m, n);
+    } 
+}
+Zadacha64();
